085eb9cbd20fe8a3ce023dfef0ea271dab770685
Milan Ugrin <ugrin@nittin.cz>
2026-09-07 19:21:43 +0200

    attempt: HP featured articles bg

--- ui/.eleventy.js
+++ ui/.eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/uploads");
 
   // Date filters
   eleventyConfig.addFilter("isoDate", (d) => new Date(d).toISOString().slice(0, 10));
